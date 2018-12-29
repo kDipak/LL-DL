96deb85089a4a231176db932f3efdaeb0db45574
Dipak Gangaram <dpk1982@gmail.com>
2018-12-29 20:35:12 +0100

    Script rewritten.
    new filter to select video url.

--- ll-dl.ps1
+++ ll-dl.ps1
@@ -1,46 +1,52 @@
-#Remove-Variable * -ErrorAction SilentlyContinue
-#Import-Module Communary.PASM #Fuzzy Logic Search module
+if (Get-Module -ListAvailable -Name Communary.PASM) {
+    Write-Host "Module exists"
+     } 
+else {
+     Install-Module Communary.PASM
+     }
+Import-Module Communary.PASM
 cls
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
-#your LinkedIN username
-$usr  = ""
-
-# LinkedIn Password in a text file, change path.
+$usr  = Get-Content -Path "D:\Download\Youtubedl\un.txt"
 $pwd  = Get-Content -Path "D:\Download\Youtubedl\pw.txt" | ConvertTo-SecureString -asPlainText -Force
-
-#LinkedIn Password used in Youtube-dl
 $pwdp = Get-Content -Path "D:\Download\Youtubedl\pw.txt"
 $cred = New-Object System.Management.Automation.PSCredential($usr,$pwd)
-
-# path to course, going to change thos so it reads the uri from a text file.
-$uri  = "https://www.linkedin.com/learning/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-
-#Location of youtube-dl binaries
-$ytdl = 'D:\Download\Youtubedl'
-
-# Get all links on the course page
-$connect = Invoke-WebRequest -Uri $uri -Credential $cred
-$Links = $connect.Links.Href
-$Links = $Links 
-$Links = @($Links) -inotlike '*Login*' -inotlike '*course_preview*' -ilike 'https*'
-$Links = $Links | ForEach-Object {
-$_ -replace "\?autoplay=true&amp;trk=course_tocItem","" 
-}
-
-#Get the Page title and prepare it for fuzzymatching the URL
-$connect.Content -match "<title>(?<title>.*)</title>" | out-null
-$RawTitle = $matches['title']
-$Clean = $RawTitle | ForEach-Object {
-$_  -replace "# ", "-sharp-" -replace "  ","-" -replace ":", "" -replace " ", "-" -replace ",", ""}
-
-#Fuzze Match url to page title.
-
-$S1 = $links | Select-FuzzyString $Clean | Sort-Object Score,Result -Descending | Select Result
-
-#Start Downloading
-$dl = $S1 | ForEach-Object {
-cd $ytdl;
-Write-Host 'Getting: ' $_.Result
-.\youtube-dl.exe -f best $_.Result  -u $usr -p $pwdp
+$url  = Get-content -Path "D:\Download\Youtubedl\dlthis.txt"
+$ytdl = "D:\Download\Youtubedl"
+$start = $url | ForEach-Object {
+    write-host "processing url" $_
+    $n = 0 
+    $connect = Invoke-WebRequest -Uri $_ -Credential $cred
+    $L1 = $connect.Links.Href
+    $L2 = @($L1) -ilike "*?autoplay=true&amp;trk=course_tocItem"
+    $L3 = $L2 | ForEach-Object { $_ -replace "\?autoplay=true&amp;trk=course_tocItem","" }
+    $RawTitle =$connect.ParsedHtml | Select nameprop |  ForEach-Object { $_ -replace "\.","-" -replace "\(" ,"" -replace "\)" ,"" -replace "#:" ,"-sharp" -replace "#"  , "-sharp" -replace "  " ,"-" -replace ":"  , "" -replace " "  , "-" -replace ","  , "" -replace "@{nameProp=", "" -replace "}", ""}
+    $folder = $connect.ParsedHtml | Select nameprop | ForEach-Object { $_  -replace "  ","" -replace ":", "" -replace "@{nameProp=", "" -replace "}", ""}
+    $dl = $L3 |  Select-FuzzyString $RawTitle | Select Result | 
+    ForEach-Object {
+    cd $ytdl; $i = 1 + $n 
+    $i | foreach {
+                 $ii = "{0:00}" -f $_
+                 }
+    $path = $ytdl+'\'+$folder
+    If(!(test-path $path))
+    {
+    New-Item -ItemType Directory -Force -Path $path
+    }
+    cd $path
+    $vbeg = $_.Result.LastIndexOf('/')+1
+    $filepart = $_.Result.Substring($vbeg)
+    $filename = "$($ii) $($filepart).mp4"
+    $filedir = (Get-Item -Path ".\").FullName
+    $filepath = Join-Path $filedir $filename
+If(!(test-path $filepath))
+     {
+      Write-Host 'Downloading = ' $_ "to file" $filename -ForegroundColor Green
+      ..\youtube-dl.exe -f best $_.Result  -o $filename --print-traffic --cookies ..\cookie.txt | Out-Host
+     } 
+else {
+     Write-Host 'File:' $filename 'already exists' -ForegroundColor Yellow 
+   }
+  $n = $i ++
+ }
 }
